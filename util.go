8694b01e07293027deeb9e38f9e1eff765fc0a90
Pjdur <pjdurable@gmail.com>
2025-12-14 07:15:21 +0000

    Patch

--- util.go
+++ util.go
@@ -7,23 +7,3 @@ func drawString(x, y int, fg, bg termbox.Attribute, msg string) {
 		termbox.SetCell(x+i, y, r, fg, bg)
 	}
 }
-
-func (i *Input) Draw(t *term.Term, x, y, w, h int) {
-	// Draw title
-	if i.title != "" {
-		t.DrawText(x, y, i.title, style.New())
-		y++
-	}
-
-	for idx, r := range i.value {
-		if idx == i.cursor {
-			t.DrawRune(x+idx, y, r, i.cursorStyle)
-		} else {
-			t.DrawRune(x+idx, y, r, i.style)
-		}
-	}
-
-	if i.cursor == len(i.value) {
-		t.DrawRune(x+i.cursor, y, ' ', i.cursorStyle)
-	}
-}
